da8d891390f65973ff936fe33cb50faa28427d9f
hongjungeun <gjhong1129@gmail.com>
2020-12-21 15:15:19 -0800

    1221_홍준근_setNumber_mothod_modified_3

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/FlutterLocalNotificationsPlugin.java
@@ -223,7 +223,7 @@ public class FlutterLocalNotificationsPlugin implements MethodCallHandler, Plugi
     }
     private static void setNumber(NotificationDetails notificationDetails , NotificationCompat.Builder builder) {
         Log.d("NotificationBadgeCount1",notificationDetails.badgeCount.toString() +" : "+notificationDetails.id.toString());
-        Log.d("NotificationBadgeCount2",(notificationDetails.id==0)+" : "+notificationDetails.setAsGroupSummary.toString());
+        Log.d("NotificationBadgeCount2",(notificationDetails.id==0)+" : "+notificationDetails.setAsGroupSummary);
         if(notificationDetails.id==0&&notificationDetails.setAsGroupSummary)builder.setNumber(notificationDetails.badgeCount);
     }
     private static void setSmallIcon(Context context, NotificationDetails notificationDetails, NotificationCompat.Builder builder) {
